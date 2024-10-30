8fb33875a1679c95a8630fab94fd4e26af87edde
amstokely <andystokely@gmail.com>
2024-10-30 10:23:53 -0600

    Moved introduction text from home section into a new file, home.js, in the sections directory. This will allow the Panda-C team to easily update this section, with a most likely better introduction statement.

---
+++ frontend/src/sections/home.js
@@ -0,0 +1,4 @@
+export const home = {
+    introduction: "   A community data assimilation system for the Model for Prediction " +
+        "Across Scales - Atmospheres based upon the Joint Effort for Data assimilation Integration.",
+}

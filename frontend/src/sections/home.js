0d8be2021f53abcfe823cda048f2c586aaa6c810
amstokely <andystokely@gmail.com>
2024-10-30 10:44:41 -0600

    Changed css so that the learn more button on the home page is centered when the adjacent introduction text is longer than 2 lines.

--- frontend/src/sections/home.js
+++ frontend/src/sections/home.js
@@ -1,4 +1,3 @@
 export const home = {
-    introduction: "   A community data assimilation system for the Model for Prediction " +
-        "Across Scales - Atmospheres based upon the Joint Effort for Data assimilation Integration.",
+    introduction: "MPAS-JEDI is NCAR/MMM's new-generation community data assimilation system for the Model for Prediction Across Scales - Atmosphere (MPAS-A) based upon the Joint Effort for Data assimilation Integration (JEDI). MPAS-JEDI allows both global and regional applications with deterministic or ensemble data assimilation algorithms. Advanced all-sky satellite radiance data assimilation capability is also enabled in MPAS-JEDI."
 }

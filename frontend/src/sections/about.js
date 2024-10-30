edf4c88938f75ba563fc66dd88a838a2a80e116f
amstokely <andystokely@gmail.com>
2024-10-30 16:06:19 -0600

    Added tutorial to README that goes over how to recreate the website, in terms of content, from scratch.

--- frontend/src/sections/about.js
+++ frontend/src/sections/about.js
@@ -10,5 +10,4 @@
  * }
  */
 export const about = {
-    body: "MPAS-JEDI is NCAR/MMM's new-generation community data assimilation system for the Model for Prediction Across Scales - Atmosphere (MPAS-A) based upon the Joint Effort for Data assimilation Integration (JEDI). MPAS-JEDI allows both global and regional applications with deterministic or ensemble data assimilation algorithms. Advanced all-sky satellite radiance data assimilation capability is also enabled in MPAS-JEDI."
 }
--- frontend/src/sections/publications.js
+++ frontend/src/sections/publications.js
@@ -33,19 +33,4 @@
  * }
  */
 export const publications = [
-    {
-        title: "Data assimilation for the Model for Prediction Across Scales – Atmosphere with the Joint Effort for Data assimilation Integration (JEDI-MPAS 1.0.0): EnVar implementation and evaluation",
-        url: "https://gmd.copernicus.org/articles/15/7859/2022/"
-    },
-    {
-        title: "Data assimilation for the Model for Prediction Across Scales – Atmosphere with the Joint Effort for Data assimilation Integration (JEDI-MPAS 2.0.0-beta): ensemble of 3D ensemble-variational (En-3DEnVar) assimilations",
-        url: "https://gmd.copernicus.org/articles/16/7123/2023/",
-        summary: "We describe the multivariate static background error covariance (B) for the JEDI-MPAS 3D-Var data assimilation system. With tuned B parameters, the multivariate B gives physically balanced analysis increment fields in the single-observation test framework. In the month-long cycling experiment with a global 60 km mesh, 3D-Var with static B performs stably. Due to its simple workflow and minimal computational requirements, JEDI-MPAS 3D-Var can be useful for the research community."
-    },
-    {
-        title: "Three-dimensional variational assimilation with a multivariate background error covariance for the Model for Prediction Across Scales – Atmosphere with the Joint Effort for Data assimilation Integration (JEDI-MPAS 2.0.0-beta)",
-        url: "https://gmd.copernicus.org/articles/17/3879/2024/",
-        date: "2024",
-        authors: ["Byoung-Joo Jung","Benjamin Ménétrier", "Chris Snyder", "Zhiquan Liu", "Jonathan J. Guerrette", "Junmei Ban", "Ivette Hernández Baños", "Yonggang G. Yu", "William C. Skamarock"]
-    },
 ];
